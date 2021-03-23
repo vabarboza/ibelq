e699e07114c0951c21d3f659987d4a0325b7cfe5
Vinicius Araujo <dev.viniciusbarboza@gmail.com>
2021-03-22 22:53:35 -0300

    Favicon

--- resources/views/layouts/basico.blade.php
+++ resources/views/layouts/basico.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <title>@yield('titulo')</title>
     <!-- MDB icon -->
-    <link rel="icon" href="{{ asset('img/mdb-favicon.ico') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon" />
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
     <!-- Google Fonts Roboto -->
